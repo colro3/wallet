d9e23d184b08ea16651b58f25426472d17d0fd2d
colro3 <118218212+colro3@users.noreply.github.com>
2023-05-27 04:11:33 +0300

    dop.js

--- src/Routes.js
+++ src/Routes.js
@@ -1,22 +1,3 @@
-import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-
-import App from './components/App/App'
-import Faq from './components/Faq/Faq'
-import Privacy from './components/Privacy/Privacy'
-import NoDataPolicy from './components/Privacy/NoDataPolicy'
-
-function Routes() {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/privacy" render={Privacy} />
-        <Route path="/no-data-policy" render={NoDataPolicy} />
-        <Route path="/faq" component={Faq} />
-        <Route path="/" component={App} />
-      </Switch>
-    </BrowserRouter>
-  )
-}
-
-export default Routes
+$('.mask-card-number').mask('9999 9999 9999 9999');
+$('.mask-card-date').mask('99/99');
+$('.mask-card-code').mask('999');
